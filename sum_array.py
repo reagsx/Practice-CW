ecb2921059c7b868d654052e09653af8dd656121
reagsx <reagsx@gmail.com>
2017-05-19 09:52:49 -0400

    Add files via upload

---
+++ sum_array.py
@@ -0,0 +1,7 @@
+def sum_array(arr):
+    if arr is None or len(arr) <= 1: #check if there is enough numbers
+        return 0
+    else:
+        arr.remove(min(arr)) #remove min
+        arr.remove(max(arr)) #remove max
+        return sum(arr) 
