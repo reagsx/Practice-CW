94e61c84ad7b8d6cc07d4cdba5c00ee3058247c3
reagsx <reagsx@gmail.com>
2017-05-22 13:48:48 -0400

    Add files via upload

---
+++ dig_pow.py
@@ -0,0 +1,19 @@
+# Playing with Digits 
+
+
+def dig_pow(n, p):
+    # your code
+    count = 0
+    v = []
+    pw = []
+    x = [int(i) for i in str(n)]
+    for i in x:
+        v.append(i ** (p + count))
+        pw.append(p + count)
+        count += 1  
+    k = sum(v)/n
+
+    if sum(v)%n == 0:
+        return k
+    else:
+        return -1
---
+++ is_square.py
@@ -0,0 +1,3 @@
+import math
+def is_square(n):    
+        return True if n > 0 and math.sqrt(n).is_integer() else False
