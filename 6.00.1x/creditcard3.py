fdb0809bae0e4199a6e61fc213f5d842289d98f8
Chris Reagle <reagsx@gmail.com>
2017-06-12 18:23:43 -0400

    finished creditcard 3

--- 6.00.1x/creditcard3.py
+++ 6.00.1x/creditcard3.py
@@ -12,17 +12,16 @@ epsilon = .01
 
 # Calculate Here While Loop is Broken, need to include interest
 while True:
-    print(payment, upper, lower)
     nb = balance
 
     for i in range(12):
         nb = (nb - payment) * (1 + monthly_interest)
-    print(nb)
-    if nb < 0:
+
+    if nb < 0 and abs(nb) > epsilon:
         upper = payment
-    elif nb > 0:
+    elif nb > 0 and abs(nb) > epsilon:
         lower = payment
-    elif nb - balance >= epsilon:
+    else:
         break
 
     payment = (upper + lower) / 2
