386b9f57c52b25cbc212d01c75cd5065612d95a9
Chris Reagle <reagsx@gmail.com>
2017-06-12 13:01:43 -0400

    Number Guess

---
+++ 6.00.1x/guessnumber.py
@@ -0,0 +1,24 @@
+# Gather inputs, test high and low
+print("Please enter number 0 - 99:")
+
+high = 100
+low =0
+guess = 50
+v = ''
+
+while v != 'c':
+    print('Is your number:', guess, '?')
+    v = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. ")
+    if v == 'h':
+        high = guess
+        guess = int((high+low)/2)
+    elif v == 'l':
+        low = guess
+        guess = int((high+low)/2)
+    elif v == 'c':
+        #score!
+        v = 'c'
+    else:
+        v = input("Input not understood, please re-enter 'h' for too high, 'l' for too low, or 'c' if I guessed correctly. ")
+
+print('Game over! Your secret number was: ' + str(guess))
